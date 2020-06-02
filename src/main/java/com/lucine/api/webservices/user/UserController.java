726fbd3287621c30fa0a3aadb47cd34bcd8fb66b
mydnac <mandycoelho5@gmail.com>
2020-06-03 00:00:32 +0200

    create getMapping users

--- src/main/java/com/lucine/api/webservices/user/UserController.java
+++ src/main/java/com/lucine/api/webservices/user/UserController.java
@@ -1,4 +1,22 @@
 package com.lucine.api.webservices.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired UserService service;
+
+    @GetMapping()
+    public List<User> getAll() {
+        return this.service.getAll();
+    }
+
+
 }
