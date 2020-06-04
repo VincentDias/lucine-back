53f4a4fe676460b98d7e5b753276aa5c59b95d53
mydnac <mandycoelho5@gmail.com>
2020-06-04 18:06:54 +0200

    feat(user): method authUser(userLogin)

--- src/main/java/com/lucine/api/webservices/user/UserService.java
+++ src/main/java/com/lucine/api/webservices/user/UserService.java
@@ -17,4 +17,8 @@ public class UserService {
         return repository.findAll();
     }
 
+    public User authUser(String userLogin) {
+        return this.repository.authUser(userLogin);
+    }
+
 }
