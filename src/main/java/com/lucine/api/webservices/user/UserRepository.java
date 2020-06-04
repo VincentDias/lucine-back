6d021d0e8ea76b7a04e0fb01b2e12f48250ab11e
mydnac <mandycoelho5@gmail.com>
2020-06-04 18:05:56 +0200

    feat(user):select one user by id

--- src/main/java/com/lucine/api/webservices/user/UserRepository.java
+++ src/main/java/com/lucine/api/webservices/user/UserRepository.java
@@ -12,7 +12,7 @@ public class UserRepository {
     private final static String DB_USER = System.getenv("lucine_db_user");
     private final static String DB_PASSWORD = System.getenv("lucine_db_password");
 
-    public List<User> findAll(){
+    public List<User> findAll() {
 
         List<User> users = new ArrayList<>();
 
@@ -34,7 +34,7 @@ public class UserRepository {
                 String login = resultSet.getString("login");
                 String password = resultSet.getString("password");
                 String lastName = resultSet.getString("lastname");
-                String firstName = resultSet.getString ("firstname");
+                String firstName = resultSet.getString("firstname");
                 String role = resultSet.getString("role");
                 String mail = resultSet.getString("mail");
                 users.add(new User(id, login, password, lastName, firstName, role, mail));
@@ -46,5 +46,42 @@ public class UserRepository {
         }
         return users;
     }
+
+    public User authUser(String userLogin) {
+
+        User currentUser = null;
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "SELECT * FROM User where id = 1"
+            );
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                String lastName = resultSet.getString("lastname");
+                String firstName = resultSet.getString("firstname");
+                String role = resultSet.getString("role");
+                String mail = resultSet.getString("mail");
+                currentUser = new User(id, login, password, lastName, firstName, role, mail);
+            }
+            resultSet.close();
+            return currentUser;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        }
+    return null;
+    }
 }
+
 
