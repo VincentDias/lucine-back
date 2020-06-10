2eb2baa6bb48cc6f70ee129556e948ca4a99ab9c
Clement <clement.cheradame@free.fr>
2020-06-10 13:45:08 +0200

    feat(youtube): fixing http request for youtube search api

--- src/main/java/com/lucine/api/webservices/youtube/search/YoutubeController.java
+++ src/main/java/com/lucine/api/webservices/youtube/search/YoutubeController.java
@@ -3,20 +3,18 @@ package com.lucine.api.webservices.youtube.search;
 
 import com.lucine.api.webservices.youtube.search.models.YoutubeVideo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class YoutubeController {
 
     @Autowired
     YoutubeService service;
 
+
     @GetMapping("/ytbSearch")
-    public YoutubeVideo getYoutubeVideos(@RequestBody List<String> keywords) {
+    public YoutubeVideo getYoutubeVideos(@RequestParam String keywords) {
 
         return service.getYoutubeVideos(keywords);
     }
--- src/main/java/com/lucine/api/webservices/youtube/search/YoutubeService.java
+++ src/main/java/com/lucine/api/webservices/youtube/search/YoutubeService.java
@@ -5,12 +5,10 @@ import com.lucine.api.webservices.youtube.search.models.YoutubeVideo;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.List;
-
 @Service
 public class YoutubeService {
 
-    public YoutubeVideo getYoutubeVideos(List<String> keywords) {
+    public YoutubeVideo getYoutubeVideos(String keywords) {
         WebClient webClient = WebClient
                 .builder()
                 .baseUrl("https://www.googleapis.com/youtube/v3")
@@ -21,7 +19,7 @@ public class YoutubeService {
                 .uri(UriBuilder -> UriBuilder
                         .path("/search")
                         .queryParam("part", "snippet")
-                        .queryParam("maxResults", 1)
+                        .queryParam("maxResults", 5)
                         .queryParam("q", keywords)
                         .queryParam("key", "AIzaSyBiT2_dE36crA2m4jeXNX20fDSDS7uXq1A")
                         .build())
